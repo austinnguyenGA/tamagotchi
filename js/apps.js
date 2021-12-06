ed056c67aa2cacc100380967eb3ebcc0900a9f77
Austin Nguyen <austinnguyen@MacBook-Pro.local>
2021-12-06 14:26:47 -0600

    started adding the core html and css. Very little java script has been done at this point.

--- js/apps.js
+++ js/apps.js
@@ -1,2 +1,18 @@
-console.log("hi")
-console.log("test")
+console.log("tamagotchi")
+
+// make class for tamagotchi
+
+class Tamagotchi {
+  constructor() {
+    this.name = name
+    this.age = 0
+    this.hunger = 10
+    this.bordem = 10
+    this.sleepiness = 10
+  }
+}
+
+const pet = new Tamagotchi ()
+
+
+//document.body.style.image
