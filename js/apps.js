436da560c2d777a36f0dc15755d58ac790aaac18
Austin Nguyen <austinnguyen@MacBook-Pro.local>
2021-12-07 16:53:39 -0600

    Created the functions that allow the buttons to function as intended. Added the functions that change the background depending on the situation.

--- js/apps.js
+++ js/apps.js
@@ -7,84 +7,194 @@ class Tamagotchi {
     this.name = name
     this.age = age
     this.hunger = hunger
-    this.bordom = boredom
-    this.energy = energy
+    this.boredom = boredom
+    this.sleepiness = sleepiness
   }
 }
 
 class Pain extends Tamagotchi {
-  constructor() {
-  super(name, age, hunger, boredom, sleepiness)
-  this.name = name
-  this.age = 0
-  this.hunger = 10
-  this.boredom = 10
-  this.energy = 10
+  constructor(name) {
+    super(name)
+    this.name = name
+    this.age = 0
+    this.hunger = 10
+    this.boredom = 10
+    this.sleepiness = 10
   }
 }
-const game = {
-  timer: 20,
+
+ const game = {
+  timer: 10,
   age: 0,
   hunger: 10,
   boredom: 10,
-  energy: 10,
-  gettingHungry() {},
-  gettingSleepy() {},
-  gettingBored() {},
+  sleepiness: 10,
+  gettingHungry() {
+    const hungerTimer = document.querySelector('#foodTimer')
+
+    this.hunger --
+    hungerTimer.innerText = `Hunger: ${this.hunger}`
+
+    const intervalID = setInterval(() => {
+      this.hunger --
+      hungerTimer.innerText = `Hunger: ${this.hunger}`
+
+      if (this.hunger <= 0) {
+        clearInterval(intervalID)
+        const backgroundElement4 = document.querySelector('#background')
+        const gameOverBackground = document.querySelector('#gameOverScreen')
+        const backgroundElement5 = document.querySelector('#chibiPain')
+        const removeButtons1 = document.querySelector('#Feed')
+        const removeButtons2 = document.querySelector('#Sleep')
+        const removeButtons3 = document.querySelector('#Play')
+        const removeTimer1 = document.querySelector('#foodTimer')
+        const removeTimer2 = document.querySelector('#sleepTimer')
+        const removeTimer3 = document.querySelector('#boredomTimer')
+          backgroundElement4.classList.add('gameOverScreen')
+          backgroundElement4.classList.remove('daytimeBackground')
+          backgroundElement5.classList.remove('chibiPain')
+          removeButtons1.classList.remove('button')
+          removeButtons2.classList.remove('button')
+          removeButtons3.classList.remove('button')
+          removeTimer1.classList.remove('timer')
+          removeTimer2.classList.remove('timer')
+          removeTimer3.classList.remove('timer')
+      }
+    }, 3000)
+  },
+
+  gettingSleepy() {
+    const sleepinessTimer = document.querySelector('#sleepTimer')
+
+    this.sleepiness --
+    sleepinessTimer.innerText = `Sleepiness: ${this.sleepiness}`
+
+    const intervalID = setInterval(() => {
+      this.sleepiness --
+      sleepinessTimer.innerText = `Sleepiness: ${this.sleepiness}`
+
+      if (this.sleepiness <= 0) {
+        clearInterval(intervalID)
+        const backgroundElement4 = document.querySelector('#background')
+        const gameOverBackground = document.querySelector('#gameOverScreen')
+        const backgroundElement5 = document.querySelector('#chibiPain')
+        const removeButtons1 = document.querySelector('#Feed')
+        const removeButtons2 = document.querySelector('#Sleep')
+        const removeButtons3 = document.querySelector('#Play')
+        const removeTimer1 = document.querySelector('#foodTimer')
+        const removeTimer2 = document.querySelector('#sleepTimer')
+        const removeTimer3 = document.querySelector('#boredomTimer')
+          backgroundElement4.classList.add('gameOverScreen')
+          backgroundElement4.classList.remove('daytimeBackground')
+          backgroundElement5.classList.remove('chibiPain')
+          removeButtons1.classList.remove('button')
+          removeButtons2.classList.remove('button')
+          removeButtons3.classList.remove('button')
+          removeTimer1.classList.remove('timer')
+          removeTimer2.classList.remove('timer')
+          removeTimer3.classList.remove('timer')
+      }
+    }, 3000)
+  },
+  gettingBored() {
+    const boredTimer = document.querySelector('#boredomTimer')
+
+    this.boredom --
+    boredTimer.innerText = `Energy: ${this.boredom}`
+
+    const intervalID = setInterval(() => {
+      this.boredom --
+      boredTimer.innerText = `Energy: ${this.boredom}`
+
+      if (this.boredom <= 0) {
+        clearInterval(intervalID)
+        const backgroundElement4 = document.querySelector('#background')
+        const gameOverBackground = document.querySelector('#gameOverScreen')
+        const backgroundElement5 = document.querySelector('#chibiPain')
+        const removeButtons1 = document.querySelector('#Feed')
+        const removeButtons2 = document.querySelector('#Sleep')
+        const removeButtons3 = document.querySelector('#Play')
+        const removeTimer1 = document.querySelector('#foodTimer')
+        const removeTimer2 = document.querySelector('#sleepTimer')
+        const removeTimer3 = document.querySelector('#boredomTimer')
+          backgroundElement4.classList.add('gameOverScreen')
+          backgroundElement4.classList.remove('daytimeBackground')
+          backgroundElement5.classList.remove('chibiPain')
+          removeButtons1.classList.remove('button')
+          removeButtons2.classList.remove('button')
+          removeButtons3.classList.remove('button')
+          removeTimer1.classList.remove('timer')
+          removeTimer2.classList.remove('timer')
+          removeTimer3.classList.remove('timer')
+
+      }
+    }, 3000)
+  },
+  feed(){
+    const hungerButton = document.querySelector('#Feed')
+    hungerButton.addEventListener('click', (event) => {
+      this.hunger ++
+
+    })
+  },
+  sleep(){
+    const sleepButton = document.querySelector('#Sleep')
+    sleepButton.addEventListener('click', (event) => {
+      this.sleepiness ++
+
+    })
+  },
+  play(){
+    const playButton = document.querySelector('#Play')
+    playButton.addEventListener('click', (event) => {
+      this.boredom ++
+
+    })
+  },
 }
+
 const backgroundElement = document.querySelector('#background');
-const sleepButton = document.querySelector('#sleep')
-
-//console.log(backgroundElement.classList)
-
+const sleepButton = document.querySelector('#Sleep')
 sleepButton.addEventListener('click', (event) => {
-  // console.log("go to sleep");
-  // console.log(backgroundElement.classList);
   backgroundElement.classList.add('nighttimeBackground')
-  //console.log(backgroundElement.classList);
   backgroundElement.classList.remove('daytimeBackground')
-  //console.log(backgroundElement.classList);
   setTimeout(() => {
     backgroundElement.classList.add('daytimeBackground')
     backgroundElement.classList.remove('nighttimeBackground')
-  }, 2500)
+  }, 1000)
   }
 );
 
 const backgroundElement2 = document.querySelector('#background');
-const eatButton = document.querySelector('#feed')
+const eatButton = document.querySelector('#Feed')
 
-//console.log(backgroundElement2.classList)
 
 eatButton.addEventListener('click', (event) => {
-  //console.log("go to eat");
-  //console.log(backgroundElement2.classList);
   backgroundElement.classList.add('timeToEatBackground')
-  //console.log(backgroundElement2.classList);
   backgroundElement.classList.remove('daytimeBackground')
-  //console.log(backgroundElement2.classList);
   setTimeout(() => {
     backgroundElement2.classList.add('daytimeBackground')
     backgroundElement2.classList.remove('timeToEatBackground')
-  }, 2500)
+  }, 1000)
   }
 );
 
 const backgroundElement3 = document.querySelector('#background');
-const playButton = document.querySelector('#play')
-
-//console.log(backgroundElement3.classList)
-
+const playButton = document.querySelector('#Play')
 playButton.addEventListener('click', (event) => {
-  //console.log("go to play");
-  //console.log(backgroundElement3.classList);
   backgroundElement.classList.add('playtimeBackground')
-  //console.log(backgroundElement3.classList);
   backgroundElement.classList.remove('daytimeBackground')
-  //console.log(backgroundElement3.classList);
   setTimeout(() => {
     backgroundElement3.classList.add('daytimeBackground')
     backgroundElement3.classList.remove('playtimeBackground')
-  }, 2500)
+  }, 1000)
   }
 );
+
+const newTam = new Pain("bob")
+game.gettingHungry()
+game.gettingSleepy()
+game.gettingBored()
+game.feed()
+game.sleep()
+game.play()
